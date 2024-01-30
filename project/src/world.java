dee08b40e10e4d85673bc7c970e9ffe1babe4202
praveenpriyashan <praveenpriyashan@gmail.com>
2024-01-30 20:41:13 +0530

    2st

--- project/src/world.java
+++ project/src/world.java
@@ -1,4 +1,22 @@
 void main(){
     System.out.println("hello how ar you");
 
+
+}
+public class ServiceFactory{
+    private static  ServiceFactory serviceFactory;
+    private ServiceFactory(){}
+
+    private static ServiceFactory getInstance(){
+        if (serviceFactory==null){
+            serviceFactory=new ServiceFactory();
+        }
+    }
+    public SuperService getService(ServiceType serviceType){
+        switch (serviceType){
+
+        }
+
+    }
+
 }
